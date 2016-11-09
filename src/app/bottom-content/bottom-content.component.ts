63314a12287f3d41c0f170935fdd4c0c8cf8b7b9
Josh Summerhays <russiansummer@gmail.com>
2016-11-09 12:45:24 -0700

    BUILD: bottom-content component

---
+++ src/app/bottom-content/bottom-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-content',
+  templateUrl: './bottom-content.component.html',
+  styleUrls: ['./bottom-content.component.css']
+})
+export class BottomContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
