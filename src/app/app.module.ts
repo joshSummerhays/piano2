379535f49e01a0a5c0be0feec454125c94030807
Josh Summerhays <russiansummer@gmail.com>
2016-11-09 11:44:19 -0700

    BUILD: component selectors added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { TopContentComponent } from './top-content/top-content.component';
+import { BottomContentComponent } from './bottom-content/bottom-content.component';
+import { FooterComponent } from './footer/footer.component';
+import { MiddleContentComponent } from './middle-content/middle-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    TopContentComponent,
+    BottomContentComponent,
+    FooterComponent,
+    MiddleContentComponent
   ],
   imports: [
     BrowserModule,
