f741d58779f520c7c01224fd381815d94f2c4b26
Josh Summerhays <russiansummer@gmail.com>
2016-11-15 16:59:54 -0700

    BUILD: animation preloader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  loader: boolean = true;
+
+  ngOnInit () {
+    setTimeout(
+      () => {
+        this.loader = !this.loader;
+      }, 8000
+    );
+  }
 }
