525149e653e8f993b3990a1abea8fc33e5ea9edf
Josh Summerhays <russiansummer@gmail.com>
2016-11-15 17:08:57 -0700

    BUILD: component adds

--- src/app/top-content/top-content.component.ts
+++ src/app/top-content/top-content.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopContentComponent implements OnInit {
 
-  constructor() { }
+    play: boolean = false;
+    mediaURL: string = "https://s-media-cache-ak0.pinimg.com/originals/30/9b/55/309b55739a7a2d20a9de4498ee1de059.jpg";
+
+    constructor() { }
+
+    playMovie() {
+        this.play = true;
+    }
+
+    chooseMedia (media) {
+      if (media === 'photo') {
+        this.mediaURL = "https://s-media-cache-ak0.pinimg.com/originals/30/9b/55/309b55739a7a2d20a9de4498ee1de059.jpg";
+      } else if (media === 'photo2') {
+        this.mediaURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Treble_a.svg/1280px-Treble_a.svg.png";
+      } else if (media === 'photo3') {
+        this.mediaURL = "./assets/images/play-button.png";
+      } 
+      // else if () {
+      //   this.mediaURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Treble_a.svg/1280px-Treble_a.svg.png"
+      // }
+    }
 
   ngOnInit() {
   }
