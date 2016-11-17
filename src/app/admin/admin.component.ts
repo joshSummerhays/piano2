d0f70c7dc1ee03e32cce56df8a96e9eaf329fad5
Josh Summerhays <russiansummer@gmail.com>
2016-11-17 16:32:22 -0700

    BUILD: admin comp.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-admin',
@@ -6,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  logged: boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    console.log(form.value.username);
+    console.log(form.value.password);
+    if(form.value.username === 'tsummerhays' && form.value.password === 'pianoisfun'){
+      this.logged = !this.logged;
+    }
+  }
+
+  announceSubmit(form: NgForm) {
+    console.log(form);
   }
 
 }
