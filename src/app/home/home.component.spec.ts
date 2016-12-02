98b237563916db4d7fb10a9ae67ba1bb3ad75539
Josh Summerhays <russiansummer@gmail.com>
2016-12-02 12:58:13 -0700

    BUILD: component adds and css changes

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -4,6 +4,12 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from '../header/header.component';
+import { BreadcrumbsComponent } from '../breadcrumbs/breadcrumbs.component';
+import { TopContentComponent } from '../top-content/top-content.component';
+import { BottomContentComponent } from '../bottom-content/bottom-content.component';
+import { FooterComponent } from '../footer/footer.component';
+import { MiddleContentComponent } from '../middle-content/middle-content.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -11,7 +17,15 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ 
+        HomeComponent,
+        HeaderComponent,
+        BreadcrumbsComponent,
+        TopContentComponent,
+        BottomContentComponent,
+        FooterComponent,
+        MiddleContentComponent
+         ]
     })
     .compileComponents();
   }));
--- src/app/waitlist/waitlist.component.spec.ts
+++ src/app/waitlist/waitlist.component.spec.ts
@@ -3,7 +3,11 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
 import { WaitlistComponent } from './waitlist.component';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 describe('WaitlistComponent', () => {
   let component: WaitlistComponent;
@@ -11,7 +15,14 @@ describe('WaitlistComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WaitlistComponent ]
+      declarations: [ 
+        WaitlistComponent,
+        HeaderComponent,
+        FooterComponent
+         ],
+         imports: [
+               FormsModule,
+         ]
     })
     .compileComponents();
   }));
