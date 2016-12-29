b19d5710da5bd1301ac3195722fee1a882139f28
Josh Summerhays <russiansummer@gmail.com>
2016-12-28 17:06:14 -0700

    FIX: top images

--- src/app/top-content/top-content.component.ts
+++ src/app/top-content/top-content.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class TopContentComponent implements OnInit {
 
     play: boolean = false;
-    mediaURL: string = "../assets/images/dusk-piano.jpg";
+    mediaURL: string = "./assets/images/dusk-piano.jpg";
 
     constructor() { }
 
@@ -17,13 +17,13 @@ export class TopContentComponent implements OnInit {
 
     chooseMedia (media) {
       if (media === 'photo') {
-        this.mediaURL = "../assets/images/dusk-piano.jpg";
+        this.mediaURL = "./assets/images/dusk-piano.jpg";
       } else if (media === 'photo2') {
-        this.mediaURL = "../assets/images/treble-note.png";
+        this.mediaURL = "./assets/images/treble-note.png";
       } else if (media === 'photo3') {
-        this.mediaURL = "../assets/images/piano-beginner.jpg"; 
+        this.mediaURL = "./assets/images/piano-beginner.jpg"; 
       } else if (media === 'photo4') {
-        this.mediaURL = "../assets/images/kid-piano.jpg";
+        this.mediaURL = "./assets/images/kid-piano.jpg";
       } 
       // else if () {
       //   this.mediaURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Treble_a.svg/1280px-Treble_a.svg.png"
