8404611e8afe2c8a63c4cae2534e1a790e06e6cf
Josh Summerhays <russiansummer@gmail.com>
2017-01-17 13:42:57 -0700

    server.js change

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var { mongoose } = require('./db/mongoose');
-let { Student } = require('./models/student');
+// var { mongoose } = require('./db/mongoose');
+// let { Student } = require('./models/student');
 
 
 const app = express();
+
 
 const http = require('http').createServer(app);
 const port = process.env.PORT || 3030;
@@ -13,33 +14,37 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/../dist'));
 
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../dist/index.html'));
+});
 
 //get all students on waitlist - test
-app.get('/waitlist', (req, res) => {
-    Student.find().then(waitlisters => {
-        res.status(200).send(waitlisters);
-    }, e => {
-        res.status(400).send(e)
-    });
-});
+// app.get('/waitlist', (req, res) => {
+//     Student.find().then(waitlisters => {
+//         res.status(200).send(waitlisters);
+//     }, e => {
+//         res.status(400).send(e)
+//     });
+// });
 
 //post all student to waitlist - test
-app.post('/student', (req, res) => {
-    let student = new Student({
-        name: req.body.name,
-        phone: req.body.phone,
-        email: req.body.email,
-        experience: req.body.experience,
-        startDate: req.body.startDate,
-        age: req.body.age
-    });
+// app.post('/student', (req, res) => {
+//     let student = new Student({
+//         name: req.body.name,
+//         phone: req.body.phone,
+//         email: req.body.email,
+//         experience: req.body.experience,
+//         startDate: req.body.startDate,
+//         age: req.body.age
+//     });
 
-    student.save().then(doc => {
-        res.status(201).send(doc);
-    }, e => {
-        res.status(400).send(e);
-    });
-});
+//     student.save().then(doc => {
+//         res.status(201).send(doc);
+//     }, e => {
+//         res.status(400).send(e);
+//     });
+// });
 
 
 http.listen(port, () => {
